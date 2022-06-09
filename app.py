12ef401a59efab48000a7226185cdf718fbf0532
Sergio Lucero <sergiolucero@gmail.com>
2022-06-09 10:27:23 -0400

    switched to folium

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import pandas as pd
 import streamlit as st
 import pydeck as pdk
 from datosmapa import capas
-
+from streamlit_folium import st_folium
+import folium
+##################################
 st.set_page_config(
     page_title="🍉Ferias Libres de Chile🍏", page_icon="🍅", layout="wide"
 )
@@ -10,19 +12,28 @@ st.set_page_config(
 st.title("Ferias Libres de Chile")
 
 fdf = pd.read_csv('FeriasLibresSantiago.csv')
+m = folium.Map(location=[-33.5, -70.2], zoom_start=8)
+for _, row in fdf.iterrows():
+    folium.Marker(
+        [row['Latitud'],row['Longitud']], 
+        popup=row['Horarios'], tooltip=row['Nombre']
+    ).add_to(m)
 
-selected_layers = [capas['TODAS']]
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width = 725)
 
-st.pydeck_chart(
-            pdk.Deck(
-                map_style="mapbox://styles/mapbox/light-v9",
-                initial_view_state={
-                    "latitude": -33.5,
-                    "longitude": -70.7,
-                    "zoom": 8,
-                    "pitch": 50,
-                },
-                layers=selected_layers,
-            )
-        )
+#selected_layers = [capas['TODAS']]
+
+#st.pydeck_chart(
+#            pdk.Deck(
+#                map_style="mapbox://styles/mapbox/light-v9",
+#                initial_view_state={
+#                    "latitude": -33.5,
+#                    "longitude": -70.7,
+#                    "zoom": 8,
+#                    "pitch": 50,
+#                },
+#                layers=selected_layers,
+#            )
+#        )
 st.dataframe(fdf)
