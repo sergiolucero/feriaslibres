c57f9d1b28c134bc7f0b39b4c82c4333558f106e
Sergio Lucero <sergiolucero@gmail.com>
2022-06-09 09:45:05 -0400

    Update datosmapa.py

--- datosmapa.py
+++ datosmapa.py
@@ -4,7 +4,7 @@ import pandas as pd
 capas = {'TODAS': pdk.Layer(
             "ScatterplotLayer",
             data=pd.read_csv("FeriasLibresSantiago.csv"),
-            get_position=["Longitud", "Latitud"],
+            get_position=["Latitud", "Longitud"],
             get_color=[200, 30, 0, 160],
             get_radius="[exits]",
             radius_scale=0.05,
