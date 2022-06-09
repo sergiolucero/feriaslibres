59eb47b0ae6069faebdaab6cd8276637704c44bf
Sergio Lucero <sergiolucero@gmail.com>
2022-06-09 09:40:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 import pandas as pd
 import streamlit as st
+import pydeck as pdk
+from datosmapa import capas
 
 st.set_page_config(
-    page_title="Ferias Libres de Chile", page_icon="🍅", layout="wide"
+    page_title="🍉Ferias Libres de Chile🍏", page_icon="🍅", layout="wide"
 )
 
 st.title("Ferias Libres de Chile")
 
 fdf = pd.read_csv('FeriasLibresSantiago.csv')
+
+selected_layers = [capas['TODAS']]
+
+st.pydeck_chart(
+            pdk.Deck(
+                map_style="mapbox://styles/mapbox/light-v9",
+                initial_view_state={
+                    "latitude": -33.76,
+                    "longitude": -70.4,
+                    "zoom": 11,
+                    "pitch": 50,
+                },
+                layers=selected_layers,
+            )
+        )
 st.dataframe(fdf)
